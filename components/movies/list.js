4e5091536af2e22cd95ac144558b8892b724947a
Rollietjes <rollietjes@gmail.com>
2019-11-06 12:08:41 +0000

    Added a movie table to a story

--- components/movies/list.js
+++ components/movies/list.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export default class List extends Component {
   render() {
+    let movies = this.props.movies.map((f, index) => (
+        <tr key={index}>
+            <td>{f.title}</td>
+            <td>{f.genre}</td>
+            <td>{f.duration}</td>
+        </tr>
+    ));
     return (
-      <div className="myCSSstyle">
-        <h1>Movies</h1>
-        <ul>
-          <li>THwilight</li>
-          <li>Warm Bodies</li>
-        </ul>
-      </div>
+      <div>
+        <h1>{name}</h1>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody >
+            {movies}
+          </tbody >
+      </table>
+    </div>
     );
   }
 }
--- stories/movies.js
+++ stories/movies.js
@@ -2,9 +2,21 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import List from "../components/movies/list";
-
+const movieList = "List of movies";
+const movies = [
+  {
+    title: "Twilight",
+    genre: "Romance",
+    duration: 2
+  },
+  {
+    title: "Warm Bodies",
+    genre: "Romance",
+    duration: 2
+  }
+];
 storiesOf("Movies", module)
-  .add("01 - List Movies", () => {
-    return <List />;
+  .add("01 - Table with movies", () => {
+    return <List movies={movies} name={movieList}/>;
   })
 
